ad9f8d92fd8cf8a1257e5e51dd7b2ae5a7e8d9f6
Pedro <3pedrolucca@gmail.com>
2025-08-04 09:46:48 -0300

    First Commit

---
+++ Fundamentals/PrimeiroPrograma.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CurseCSharp.Fundamentals
+{
+    public class PrimeiroPrograma
+    {
+        public static void Executar()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using CurseCSharp.Fundamentals;
+
+namespace CursoCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var central = new CentralDeExercicios(new Dictionary<string, Action>() {
+                {"Primeiro Programa - Fundamentos", PrimeiroPrograma.Executar},
+            });
+
+            central.SelecionarEExecutar();
+        }
+    }
+}
